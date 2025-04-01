35f0d6a10aa5751954b755a89661c04d40d4ac35
Maxime Buffet <167359993+MBubulti@users.noreply.github.com>
2025-04-01 17:36:46 +0200

    Presentation Contenu
    Contenu de la présentation, avec texte + img + compétences
    RESTE à ajouter logos, link, script js

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,7 +1,8 @@
 import styles from "../styles/page.module.scss";
 import '../styles/home.scss';
 import { Download } from 'lucide-react';
-
+import Image from 'next/image';
+import profilPic from '../../public/portrait_maxime.jpg'
 
 
 export default function Home() {
@@ -9,21 +10,55 @@ export default function Home() {
     
     <div className={styles.page}>
       <section className="display">
-        <article>
-          <h2>Bienvenue</h2>
-          <p>
-            Je suis un développeur fullstack débutant qui travaille sur React et son framework Next
+      <h2>Bienvenue</h2>
+        <article className="display__intro">
+          <Image className="display__intro__img" src={profilPic} alt="Photo de Maxime Buffet"/>
+          <p className="display__intro__text">
+          Passionné par l’informatique et les jeux vidéo depuis toujours, j’ai choisi de me reconvertir dans le développement, après 5 années dans l’Éducation Nationale en tant que professeur d’EPS.
+          </p>
+          <p className="display__intro__text">
+            L’aspect méthodique du développement, où chaque problème peut être décortiqué et simplifié, me passionne. J’ai envie de continuer à apprendre et j’apprécie explorer de nouveaux projets et concepts.
+Je suis également sensible aux enjeux du numérique responsable et aux bonnes pratiques qui permettent de réduire l’empreinte écologique du développement.<br/>
+          </p>
+          <p className="display__intro__text">
+            En dehors du développement, j’ai une autre passion, le sport et plus particulièrement l’Ultimate Frisbee, un sport où le collectif est une clé pour la réussite.
           </p>
         </article>
-        <article>
-        mes compétences
+        <article className="display__skills">
+        <div className="display__skills--skill">
+          <h3>Front-End</h3>
+          <ul>
+            <li>HTML</li>
+            <li>CSS et SASS</li>
+            <li>Javascript</li>
+            <li>React Native + NextJS</li>
+            <li>NPM et Vite</li>
+          </ul>
+        </div>
+        <div className="display__skills--skill">
+          <h3>Back-End</h3>
+          <ul>
+            <li>NodeJS</li>
+            <li>Express</li>
+            <li>MongoDB</li>
+            <li>API REST</li>
+          </ul>
+        </div>
+        <div className="display__skills--skill">
+          <h3>Outils développement</h3>
+          <ul>
+            <li>Git & GitHub</li>
+            <li>Postman</li>
+          </ul>
+        </div>
         </article>
-        <article>
+        <article className="display__cv">
+        <p>Vous pouvez télécharger directement mon CV</p>
         <Download/>
-        CV
         </article>
-        <article>
-        Pour en savoir plus
+        <article className="display__routes">
+        <a>Pour en savoir plus sur mes différents projets ou travaux</a><br/>
+        <a>Pour me contacter</a>
         </article>
       </section>
     </div>
